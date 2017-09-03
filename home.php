3182f17dd39c935fcd4784116d7577dc32934820
EducatedDropout <kenanthonymolina@gmail.com>
2017-09-03 23:37:30 +0800

    new

---
+++ home.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="scripts/css/v4/w3.css">
+	<title>IMWebSuite</title>
+</head>
+<style>
+body, html {
+    height: 100%;
+    margin: 0;
+}
+
+.bg {
+    /* The image used */
+
+
+    /* Full height */
+    height: 50%; 
+
+    /* Center and scale the image nicely */
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+    background-color: #b4188f
+}
+</style>
+<body>
+<div class="bg"></div>
+</body>
+</html>
